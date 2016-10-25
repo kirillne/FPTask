085f169358b0b279c3ff50b096b2ebc66b6d971c
Kiryl Lenchyk <kiryl.lenchyk@gmail.com>
2016-10-25 19:33:40 +0300

    Init db connection

--- blog/project.clj
+++ blog/project.clj
@@ -3,12 +3,16 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :plugins [[lein-ring "0.9.7"]]
+  :plugins [[lein-ring "0.9.7"]
+            [lein-localrepo "0.5.3"]]
   :ring {:handler blog.core/app
   		 :auto-reload? true
   		 :auto-refresh? false}
+  :native-path "C:/Program Files/Java/jre1.8.0_101/bin"
   :dependencies [[org.clojure/clojure "1.8.0"]
   				 [ring/ring "1.5.0"]
   				 [ring/ring-json "0.4.0"]
   				 [compojure "1.5.1"]
-  				 [hiccup "1.0.5"]])
+  				 [hiccup "1.0.5"]
+				 [org.clojure/java.jdbc "0.6.2-alpha3"]
+                 [sqljdbc4/sqljdbc4 "4.0"]])
--- blog/src/blog/core.clj
+++ blog/src/blog/core.clj
@@ -3,11 +3,13 @@
 		  ring.middleware.json
 		  ring.util.response)
 	(:require [compojure.route :as route]
-			  [blog.view :as view]))
+			  [blog.view :as view]
+						[blog.db :as db]))
 
 (defroutes app_routes
 	(GET "/" [] (view/index-page))
 	(GET "/api" [] (response {:name "Alex"}))
+	(GET "/api/users" [] (response {:value (db/get-users)}))
 	(route/resources "/"))
 
 (def app (wrap-json-response app_routes))
