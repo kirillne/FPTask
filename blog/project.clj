d86b0fd35a3d7c3fbc6be5797c638a0d5e50b80a
Kiryl Lenchyk <kiryl_lenchyk@epam.com>
2016-10-26 15:33:02 +0300

    Fix dependencies

--- blog/project.clj
+++ blog/project.clj
@@ -12,12 +12,10 @@
   :dependencies [[org.clojure/clojure "1.8.0"]
   				       [ring/ring "1.5.0"]
   				       [ring/ring-json "0.4.0"]
-                       [metosin/ring-http-response "0.8.0"]
+                 [metosin/ring-http-response "0.8.0"]
   				       [compojure "1.5.1"]
   				       [hiccup "1.0.5"]
-				       [org.clojure/java.jdbc "0.6.2-alpha3"]
-                       [selmer "1.0.9"]
-                       [sqljdbc4/sqljdbc4 "4.0"]
-                       [ring-webjars "0.1.1"]
-                       [org.webjars/bootstrap "4.0.0-alpha.3"]
-                       ])
+				         [org.clojure/java.jdbc "0.6.2-alpha3"]
+                 [selmer "1.0.9"]
+                 [sqljdbc4/sqljdbc4 "4.0"]
+])
