9f4a67600b46fe7f57828049794eea7b5a98bdf1
Alexey Drozdov <drozdikal@gmail.com>
2016-11-20 15:54:55 +0300

    Add comment page, Edit routes for comments, Add edit, delete, add for comments.

---
+++ blog/src/clj/blog/controllers/comments_controller.clj
@@ -0,0 +1,87 @@
+(ns blog.controllers.comments-controller
+	(:use 
+		[blog.dal.protocols.common-protocol]
+		[blog.dal.protocols.posts-protocol]
+		[blog.dal.protocols.profiles-protocol]
+		[blog.dal.protocols.comments-protocol])
+	(:require 
+		[blog.dal.repositories.posts-repository]
+		[blog.dal.repositories.profiles-repository]
+		[blog.dal.repositories.comments-repository]
+		[blog.layout :as layout]
+		[bouncer.core :as b]
+        [bouncer.validators :as v]
+        [blog.utils.messages-utils :as utils]
+        [ring.util.response :refer [redirect]])
+	(:import  
+		[blog.dal.repositories.posts_repository posts-repository]
+		[blog.dal.repositories.profiles_repository profiles-repository]
+		[blog.dal.repositories.comments_repository comments-repository]))
+		
+(defn add-comment-page [request post-id]
+	(layout/render
+		request "comment/comment.html" {:postid post-id})
+)
+
+(defn edit-comment-page [request info]
+	(layout/render
+		request "comment/comment.html" {:postid (:post-id info) :info info})
+)
+
+(def comment-repository (comments-repository.))
+(defn now [] (new java.util.Date))
+
+(defn add-comment [request post-id user-id]
+	(
+		do(create-item comment-repository 
+				{
+				:id nil 
+				:user-id user-id :text (get-in request [:form-params "text"]) :creation-date (now) :post-id post-id})
+		
+			(let [next-url (str "/posts/" post-id)]
+				(-> (redirect next-url))
+			)		
+	)
+)
+
+(defn edit-comment-view [request post-id id]
+	(let 
+		[
+		comment-rec (get-item comment-repository {:id id})
+		]
+		(print id)
+		(if (nil? comment-rec)
+			(->(redirect "/error"))
+			(edit-comment-page request comment-rec)
+		)
+		
+	)	
+) 
+
+(defn edit-comment [request post-id id]
+	(do
+		(
+			let
+			[
+			text (get-in request [:form-params "text"])
+			comment-rec {:text text}
+			]
+			(update-item comment-repository (assoc comment-rec :id id))
+			(let [next-url (str "/posts/" post-id)]
+				(-> (redirect next-url))
+			)
+		)
+	)
+)
+
+(defn delete-comment [request post-id id]
+	(do
+		(
+			delete-item comment-repository {:id id}
+		)
+		(let [next-url (str "/posts/" post-id)]
+				(-> (redirect next-url))
+			)
+	
+	)
+)
--- blog/src/clj/blog/routes/comments.clj
+++ blog/src/clj/blog/routes/comments.clj
@@ -1,13 +1,20 @@
 (ns blog.routes.comments
-	(:require [blog.layout :as layout]
+	(:require 
+			[blog.layout :as layout]
             [compojure.core :refer [defroutes GET POST PUT]]
             [ring.util.http-response :as response]
-            [clojure.java.io :as io]))
+            [clojure.java.io :as io]
+			[blog.controllers.comments-controller :as comments]))
 
 (defroutes comments-routes	
-	(GET "/comments/:post-id" [post-id] ())
+	(GET "/comments/:post-id" [post-id :as request] (comments/add-comment-page request post-id))
 	(POST "/comments/:post-id" [post-id] ())
-	(GET "/comments/:post-id/:comment-id" [post-id comment-id] ())
-	(PUT "/comments/:post-id/:comment-id" [post-id comment-id] ())
+	(POST "/comments/new/:post-id/:user-id" [post-id user-id :as request] (comments/add-comment request post-id user-id))
+	
+	(GET "/comments/:post-id/:comment-id" [post-id comment-id :as request] (comments/edit-comment-view request post-id comment-id ))
+	(POST "/comments/edit/:post-id/:comment-id" [post-id comment-id :as request] (comments/edit-comment request post-id comment-id ))
+	
+	(POST "/comments/delete/:post-id/:comment-id" [post-id comment-id :as request] (comments/delete-comment request post-id comment-id))
+	
 	(POST "/comments/rating/:comment-id" [comment-id] ())
 	(PUT "/comments/rating/:comment-id" [comment-id] ()))
